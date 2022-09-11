b5f2f95b23eac5a86bb3230246b882b7b39afe5d
unknown <piorus9196@gmail.com>
2022-09-11 14:06:35 +0200

    add enum properties

--- src/ts/model/Password.ts
+++ src/ts/model/Password.ts
@@ -1,23 +1,9 @@
-// export default class Password {
-//   length: number;
-//   uppercase: boolean;
-//   lowercase: boolean;
-//   numbers: boolean;
-//   symbols: boolean;
-//   constructor(
-//     length: number,
-//     uppercase: boolean,
-//     lowercase: boolean,
-//     numbers: boolean,
-//     symbols: boolean
-//   ) {
-//     this.uppercase = uppercase;
-//     this.length = length;
-//     this.symbols = symbols;
-//     this.lowercase = lowercase;
-//     this.numbers = numbers;
-//   }
-// }
+export enum Properties {
+  UPPERCASE = "uppercase",
+  LOWERCASE = "lowercase",
+  NUMBER = "number",
+  SYMBOL = "symbol",
+}
 export interface Property {
   [key: string]: boolean;
 }
