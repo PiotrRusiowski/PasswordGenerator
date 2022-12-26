99243e8b02b2cac972e8027f4c15324503df24b0
fifa f <piorus9196@gmail.com>
2022-12-26 22:05:22 +0100

    add rwd styles and add icons

--- src/decorator/FunctionDecorator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {value: true});
-exports.FunctionDecorator = void 0;
-const FunctionDecorator = (target, name, descriptor) => {
-  console.log(
-      "---------------------------- INSIDE DECORATOR ----------------------------"
-  );
-};
-exports.FunctionDecorator = FunctionDecorator;
